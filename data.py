7463e9ca4ebc0188274f4eb45c4a40be1dd5214d
isaaczacck <isaacjohnelnar@gmail.com>
2024-08-25 21:29:28 +0800

    data added

---
+++ data.py
@@ -0,0 +1,17 @@
+# variable name
+name = "John"
+
+# variable age
+age = 21
+
+# variable boolean
+is_student = True
+
+# variable float
+height = 5.4
+
+# Print all variables
+print("Name:", name)
+print("Age:", age)
+print("Is a student:", is_student)
+print("Height:", height)
